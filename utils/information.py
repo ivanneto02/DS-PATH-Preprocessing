5e58efff31e5e3a2a941ca22fe2c4c83f04d7716
ivanneto02 <ivan.a.neto@hotmail.com>
2022-07-07 02:34:10 -0700

    Changing parameters

--- utils/information.py
+++ utils/information.py
@@ -1,4 +1,5 @@
 DATA_PATH = "D:\\Documents\\Research\\DSPATH\\data"
 OUT_FILE = "scraped_preprocessed_fulltext_2.csv"
-NFILES = None # Use `None` for all the files, please make this number > 5
+NFILES = 10 # Use `None` for all the files, please make this number > 5
 UNWEIGHT_TFIDF_FILE = "TEST2.csv" # Redundant for now, don't use it
+FEATURES = 300
